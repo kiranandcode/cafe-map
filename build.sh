733f0e5e8a3ccd0ff6515c10e7c284ac5c767073
Kiran Gopinathan <mail@kirancodes.me>
2025-10-14 21:25:55 -0400

    updated build source

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ CSS_FILE=$(find dist/assets -type f -name "*.css" | head -n 1)
 JS_FILE=$(find dist/assets -type f -name "*.js" | head -n 1)
 
 # Copy them to root with fixed names
-cp "$CSS_FILE" ./cafehopper.css
-cp "$JS_FILE" ./cafehopper.js
+cp "$CSS_FILE" /Users/kirang/Documents/code/kirancodes.me/js//cafehopper.css
+cp "$JS_FILE" /Users/kirang/Documents/code/kirancodes.me/js/cafehopper.js
 
 echo "Copied:"
 echo "  CSS -> cafehopper.css"
